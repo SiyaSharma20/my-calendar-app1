f2b058226e4f3eeab4dd08ca81caeffb994ce6e9
ajay sharma <ajaysh73@gmail.com>
2023-12-03 17:24:12 -0500

    Added edit items

--- src/App.js
+++ src/App.js
@@ -6,21 +6,41 @@ import AuthenticatedView from './pages/AuthenticatedView';
 import AddItem from './components/AddItem';
 import ViewItems from './components/ViewItems';
 import SignupPage from './pages/SignupPage';
+import EditItem from './components/EditItem';
 
 function App() {
   const isUserSignedIn = !!localStorage.getItem('token');
+
+  const isAuth = false;
+  // Placeholder
+  const itemToEdit = {
+    itemName: 'Example Item to Edit',
+    itemDescription: 'Example Description to Edit',
+    selectedDate: new Date(),
+  };
+
+  // Placeholder
+  const onEditSubmit = (editedItem) => {
+    console.log('Submit edited item:', editedItem);
+  };
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/additem" element={<AddItem />} />
         <Route path="/viewitems" element={<ViewItems />} />
         <Route path="/signup" element={<SignupPage />} />
         {isUserSignedIn && (
           <Route path="authenticatedview" element={<AuthenticatedView />} />
         )}
+
+        <Route
+          path="/edititems"
+          element={
+            <EditItem itemToEdit={itemToEdit} onEditSubmit={onEditSubmit} />
+          }
+        />
       </Routes>
     </Router>
   );
