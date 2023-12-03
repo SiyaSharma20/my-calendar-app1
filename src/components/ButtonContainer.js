e6595ff455b2256092350830d96fc3b3fe16368e
ajay sharma <ajaysh73@gmail.com>
2023-12-03 14:50:27 -0500

    First commit

---
+++ src/components/ButtonContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ButtonContainer.css';
+import { useNavigate } from 'react-router-dom';
+
+const ButtonContainer = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="button-container">
+      <button onClick={() => navigate('/viewitems')} className="new-button">
+        My Items
+      </button>
+      <button onClick={() => navigate('/additem')} className="new-button">
+        Add Item
+      </button>
+      <button className="new-button">Edit Item</button>
+      <button className="new-button">Delete Item</button>
+    </div>
+  );
+};
+
+export default ButtonContainer;
